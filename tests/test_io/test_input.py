64c68e22a614b9520316b1e33b4a7ab7aff618ea
Mykhailo Pilat <m.pilat@ukma.edu.ua>
2024-03-18 22:56:23 +0200

    "Added tests of pandas_file_input function (existing file, not-existing file, empty file)."

--- tests/test_io/test_input.py
+++ tests/test_io/test_input.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+import pandas.errors
 import app.io.input as inp
 
 
@@ -27,6 +28,33 @@ class DefaultFileInput(unittest.TestCase):
         project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
         filename = os.path.join(project_dir, 'data', 'empty_file.csv')
         self.assertEqual(inp.default_file_input(filename), "")
+
+
+class PandasFileInput(unittest.TestCase):
+
+    def test_exist_file(self):
+        """Check to see if an existing file is being read correctly. (with pandas)
+        """
+        project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
+        filename = os.path.join(project_dir, 'data', 'test_table.csv')
+        expected_text = ['Hello', 'World', "It's", 'me', '!']
+        self.assertEqual(list(inp.pandas_file_input(filename)), expected_text)
+
+    def test_not_exist_file(self):
+        """Check to see if a non-existent file has been detected correctly. (with pandas)
+        """
+        project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
+        filename = os.path.join(project_dir, 'data', 'test_table_new.csv')
+        with self.assertRaises(FileNotFoundError):
+            inp.pandas_file_input(filename)
+
+    def test_empty_file(self):
+        """Check for correct processing of an empty file. (with pandas)
+        """
+        project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
+        filename = os.path.join(project_dir, 'data', 'empty_file.csv')
+        with self.assertRaises(pandas.errors.EmptyDataError):
+            inp.pandas_file_input(filename)
 
 
 if __name__ == '__main__':
